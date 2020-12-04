26d93eb384710e7331c8c4a2b95ffe921138ae2a
P.S Yan <1050335889@sjtu.edu.cn>
2020-12-05 00:18:44 +0800

    solve the read problem of dng files

--- OpenCV_Sample/run.py
+++ OpenCV_Sample/run.py
@@ -5,27 +5,66 @@ from PIL.ExifTags import TAGS
 import PIL.ExifTags
 import cv2 as cv
 import numpy as np
-import filetype
+import os.path
+import exifread
+import rawpy
+
+# RAW to BGR
+def readRawImage(filename):
+    raw = rawpy.imread(filename)
+    img = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=False, output_bps=8)
+
+    rgb = np.float32(img / 65535.0*255.0)
+    rgb = np.asarray(rgb,np.uint8)
+
+    file_name = os.path.splitext(filename)[0]
+    file_name = os.path.split(file_name)[1]
+    # print(file_name)
+    cv.imwrite('./mid/rgb_'+file_name+'.jpg', img)
+    cv.imwrite('./mid/'+file_name+'.jpg', img)
+    # with rawpy.imread(filename) as raw:
+    #     rgb = raw.postprocess()
+    # # Changing to BGR
+    # rgb[:, :, 0], rgb[:, :, 2] = rgb[:, :, 2], rgb[:, :, 0]
+    # cv.imwrite('./mid/changed_'+filename, rgb)
+    return img
+
+# retrieve file extension
+def file_extension(path):
+    return os.path.splitext(path)[1]
 
 # #读取文件夹下文件
 def ListFiles(FilePath):
     onlyfiles = [f for f in listdir(FilePath) if isfile(join(FilePath, f))]
     return onlyfiles
 
-#获得图像文件属性
-def get_exif(fn):
-    img = Image.open(fn)
-    exif = {PIL.ExifTags.TAGS[k]: v
-            for k, v in img._getexif().items()
-            if k in PIL.ExifTags.TAGS
-            }
-    return exif
-
 #获得图像曝光时间
 def get_exposure_time(fn):
-    exif = get_exif(fn)
-    exposure_time = exif.get('ExposureTime')
-    return exposure_time
+    # print(file_extension(fn))
+    if(file_extension(fn)=='.jpg'):
+        img = Image.open(fn)
+        exif = {PIL.ExifTags.TAGS[k]: v
+                for k, v in img._getexif().items()
+                if k in PIL.ExifTags.TAGS
+                }
+        return exif.get('ExposureTime')
+    else:
+        raw_file = open(fn, 'rb')
+        exif_file = exifread.process_file(raw_file, details=False, strict=True)
+        # print(exif_file)
+
+        if('EXIF ExposureTime' in exif_file.keys()):
+            exposure_str = exif_file['EXIF ExposureTime'].printable
+        else:
+            exposure_str = exif_file['Image ExposureTime'].printable
+
+        if '/' in exposure_str:
+            fenmu = float(exposure_str.split('/')[0])
+            fenzi = float(exposure_str.split('/')[-1])
+            exposure = fenmu / fenzi
+        else:
+            exposure = float(exposure_str)
+        return exposure
 
 #获取图像曝光时间序列和图像
 def getImageStackAndExpos(folderPath):
@@ -36,7 +75,10 @@ def getImageStackAndExpos(folderPath):
     for file in files:
         filePath = join(folderPath,file)
         exposTime = get_exposure_time(filePath)
-        currImage = cv.imread(filePath)
+        if(file_extension(filePath)=='.jpg'):
+            currImage = cv.imread(filePath)
+        elif(file_extension(filePath)=='.dng'):
+            currImage = readRawImage(filePath)
         exposTimes.append(exposTime)
         imageStack.append(currImage)
         filenames.append(file)

ad901a33a6132c62bb3109f35564e388e5f71e8f
P.S Yan <1050335889@sjtu.edu.cn>
2020-12-25 23:37:56 +0800

    add niqe output

--- MultiView_HDR/run.py
+++ MultiView_HDR/run.py
@@ -3,11 +3,13 @@ from pylab import *
 
 import alignment
 import getImg
+import NIQE
+
 
 if __name__ == '__main__':
 
-    fileFolderPath = './test_image/myTest/'
-    exposTimes,images,filenames = getImg.getImageStackAndExpos(fileFolderPath)
+    fileFolderPath = './test_image/Hall/'
+    # exposTimes,images,filenames = getImg.getImageStackAndExpos(fileFolderPath)
 
     # mergeMertens = cv2.createMergeMertens()
     # exposureFusion = mergeMertens.process(images)
@@ -15,7 +17,10 @@ if __name__ == '__main__':
 
     exposTimes,image_list,filenames = getImg.getImageStackAndExpos(fileFolderPath)
 
-    images, exposTimes = alignment.process(image_list,exposTimes,'AKAZE')
+    print(len(image_list))
+
+    images, exposTimes = alignment.process(image_list,exposTimes,'SIFT')
+
     print(len(images))
 
     exposTimes = np.array(exposTimes,dtype=np.float32) #convert to numpy float
@@ -26,6 +31,7 @@ if __name__ == '__main__':
     responseDebevec = calibrateDebevec.process(images, times=exposTimes)  #获得CRF
     mergeDebevec = cv2.createMergeDebevec()
     hdrDebevec = mergeDebevec.process(images, times=exposTimes.copy(), response=responseDebevec.copy()) #
+    print("HDR NIQE score is ", NIQE.niqe(hdrDebevec))
     # Save HDR image.
     cv2.imwrite("./res/hdrDebevec.hdr", hdrDebevec)
 
@@ -35,15 +41,18 @@ if __name__ == '__main__':
     tonemapDrago = cv2.createTonemapDrago(1.0, 0.7)
     ldrDrago = tonemapDrago.process(hdrDebevec)
     ldrDrago = 3 * ldrDrago
+    print("ldrDrago NIQE score is ", NIQE.niqe(ldrDrago * 255))
     cv2.imwrite("./res/ldr-Drago.jpg", ldrDrago * 255)
 
     # Tonemap using Reinhard's method to obtain 24-bit color image
     tonemapReinhard = cv2.createTonemapReinhard(1.5, 0,0,0)
     ldrReinhard = tonemapReinhard.process(hdrDebevec)
+    print("ldrReinhard NIQE score is ", NIQE.niqe(ldrReinhard * 255))
     cv2.imwrite("./res/ldr-Reinhard.jpg", ldrReinhard * 255)
 
     # Tonemap using Mantiuk's method to obtain 24-bit color image
     tonemapMantiuk = cv2.createTonemapMantiuk(2.2,0.85, 1.2)
     ldrMantiuk = tonemapMantiuk.process(hdrDebevec)
     ldrMantiuk = 3 * ldrMantiuk
+    print("ldrMantiuk NIQE score is ", NIQE.niqe(ldrMantiuk * 255))
     cv2.imwrite("./res/ldr-Mantiuk.jpg", ldrMantiuk * 255)

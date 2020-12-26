ed3e1427c7c0fc194d54f17593e1d7ac0d1e384e
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-12-26 11:09:57 +0800

    Update BilateralFilter.py

--- ToneMapping/BilateralFilter.py
+++ ToneMapping/BilateralFilter.py
@@ -24,17 +24,16 @@ def Regulation(image):
     for i in range(0, x, 1):
         for j in range(0, y, 1):
             image[j][i] = (np.log(image[j][i] + bias) - np.log(minItem + bias)) / (np.log(maxItem + bias) - np.log(minItem + bias)) + bias
-            # image[j][i] = (image[j][i] - minItem) / (maxItem - minItem) + bias
     return image
+
+def gamma_correction(R, G, B):
+    inner = (pow(R, 2.2) + pow(1.5 * G, 2.2) + pow(0.6 * B, 2.2)) / (1 + pow(1.5, 2.2) + pow(0.6, 2.2))
+    return pow(inner, 1 / 2.2)
 
 # 获取RGB分量
 BlueVector = hdr[:, :, 0]
 GreenVector = hdr[:, :, 1]
 RedVector = hdr[:, :, 2]
-# 归一化
-# BlueVector = Regulation(BlueVector)
-# GreenVector = Regulation(GreenVector)
-# RedVector = Regulation(RedVector)
 # 获取亮度分量
 Lw = HSVhdr[:, :, 2]
 # 获取尺寸信息
@@ -43,24 +42,15 @@ size_x, size_y, N = size(Lw)
 bias = 0.7
 for i in range(0, size_x, 1):
     for j in range(0, size_y, 1):
-        Lw[j][i] = (20 * pow(RedVector[j][i], 2.2) + 40 * pow(GreenVector[j][i], 2.2) + pow(BlueVector[j][i], 2.2)) / 61 + bias
+        Lw[j][i] = (20 * RedVector[j][i] + 40 * GreenVector[j][i] + BlueVector[j][i]) / 61 + bias
 # 归一化
 Lw = Regulation(Lw)
-# gamma矫正，增加亮度
-# for i in range(0, size_x, 1):
-#     for j in range(0, size_y, 1):
-#         Lw[j][i] = pow(1.0 * Lw[j][i], 2.2)
 # 双边滤波获得基本层
-#base = cv2.bilateralFilter(Lw, 5, 0.02 * max(size_x, size_y), 0.4)       # #9 邻域直径，两个 75 分别是空间高斯函数标准差，灰度值相似性高斯函数标准差
-base = cv2.bilateralFilter(Lw, 5, 75, 75)       # #9 邻域直径，两个 75 分别是空间高斯函数标准差，灰度值相似性高斯函数标准差
+base = cv2.bilateralFilter(Lw, 5, 75, 75)       # #9为邻域直径，两个75分别是空间高斯函数标准差，灰度值相似性高斯函数标准差
 max_base = findMax(base)
 min_base = findMin(base)
 # 细节层
-# detail = Lw - base
-detail = [[0] * size_x] * size_y
-for i in range(0, size_x, 1):
-    for j in range(0, size_y, 1):
-        detail[j][i] = Lw[j][i] - base[j][i]
+detail = Lw - base
 # 计算增强因子，增强（压缩）基本层
 targetContrast = np.log(5)
 compressionfactor = targetContrast / (max_base - min_base)
@@ -88,14 +78,20 @@ for i in range(0, size_x, 1):
         else:
             BlueVector[j][i] = 255
 
-# for i in range(0, size_x, 1):
-#     for j in range(0, size_y, 1):
-#         Lw[j][i] = gamma_correction(RedVector[j][i], GreenVector[j][i], BlueVector[j][i])
-hdr[:, :, 0] = BlueVector
-hdr[:, :, 1] = GreenVector
-hdr[:, :, 2] = RedVector
-# HSVhdr[:, :, 2] = Lw
-# hdr = cv2.cvtColor(HSVhdr, cv2.COLOR_HSV2BGR)
+# Gamma矫正
+for i in range(0, size_x, 1):
+    for j in range(0, size_y, 1):
+        Lw[j][i] = gamma_correction(RedVector[j][i], GreenVector[j][i], BlueVector[j][i])
+# hdr[:, :, 0] = BlueVector
+# hdr[:, :, 1] = GreenVector
+# hdr[:, :, 2] = RedVector
+HSVhdr[:, :, 2] = Lw
+hdr = cv2.cvtColor(HSVhdr, cv2.COLOR_HSV2BGR)
+# 中值滤波以去除椒盐噪声
+hdr = cv2.medianBlur(hdr, 3)
+# 锐化
+sharpen_op = np.array([[-1 / 8, -1 / 8, -1 / 8], [-1 / 8, 2, -1 / 8], [-1 / 8, -1 / 8, -1 / 8]], dtype=np.float32)
+hdr = cv2.filter2D(hdr, cv2.CV_32F, sharpen_op)
 # 保存hdr
 save_hdr(hdr, "hdr.jpg")
 
